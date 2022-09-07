489a63ecf4ce1dd52afe5ed31bff8d6c95722b37
Erwan Soulas <erwan.soulas@gmail.com>
2022-09-07 15:43:14 +0200

    third commit : add foreign key to tables.

--- bdd_cine.sql
+++ bdd_cine.sql
@@ -25,7 +25,9 @@ DROP TABLE IF EXISTS user_role;
 CREATE TABLE IF NOT EXISTS user_role(
   user_role_id INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
   user_id CHAR(36) NOT NULL,
-  role_id TINYINT(1) NOT NULL
+  role_id TINYINT(1) NOT NULL,
+  CONSTRAINT fk_user_role_user_id FOREIGN KEY (user_id) REFERENCES user (user_id),
+  CONSTRAINT fk_user_role_role_id FOREIGN KEY (role_id) REFERENCES role (role_id)
 ) ENGINE = InnoDB;
 
 DROP TABLE IF EXISTS theater;
@@ -58,14 +60,17 @@ CREATE TABLE IF NOT EXISTS movie(
 DROP TABLE IF EXISTS admin;
 CREATE TABLE IF NOT EXISTS admin(
   admin_id TINYINT(2) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  user_role_id INT(11) NOT NULL
+  user_role_id INT(11) NOT NULL,
+  CONSTRAINT fk_admin_user_role_id FOREIGN KEY (user_role_id) REFERENCES user_role (user_role_id)
 ) ENGINE = InnoDB;
 
 DROP TABLE IF EXISTS manager;
 CREATE TABLE IF NOT EXISTS manager(
   manager_id TINYINT(2) NOT NULL PRIMARY KEY AUTO_INCREMENT,
   user_role_id INT(11) NOT NULL,
-  theater_id TINYINT(2) NOT NULL
+  theater_id TINYINT(2) NOT NULL,
+  CONSTRAINT fk_manager_user_role_id FOREIGN KEY (user_role_id) REFERENCES user_role (user_role_id),
+  CONSTRAINT fk_manager_theater_id FOREIGN KEY (theater_id) REFERENCES theater (theater_id)
 ) ENGINE = InnoDB;
 
 DROP TABLE IF EXISTS customer;
@@ -73,7 +78,9 @@ CREATE TABLE IF NOT EXISTS customer(
   customer_id INT(7) NOT NULL PRIMARY KEY AUTO_INCREMENT,
   customer_payment VARCHAR(25) NOT NULL,
   user_role_id INT(11) NOT NULL,
-  price_id TINYINT(1) NOT NULL
+  price_id TINYINT(1) NOT NULL,
+  CONSTRAINT fk_customer_user_role_id FOREIGN KEY (user_role_id) REFERENCES user_role (user_role_id),
+  CONSTRAINT fk_customer_price_id FOREIGN KEY (price_id) REFERENCES price (price_id)
 ) ENGINE = InnoDB;
 
 DROP TABLE IF EXISTS room;
@@ -81,7 +88,8 @@ CREATE TABLE IF NOT EXISTS room(
   room_id TINYINT(3) NOT NULL PRIMARY KEY AUTO_INCREMENT,
   room_numerotation TINYINT(2) NOT NULL,
   room_places TINYINT(3) NOT NULL,
-  theater_id TINYINT(2) NOT NULL
+  theater_id TINYINT(2) NOT NULL,
+  CONSTRAINT fk_room_theater_id FOREIGN KEY (theater_id) REFERENCES theater (theater_id)
 ) ENGINE = InnoDB;
 
 DROP TABLE IF EXISTS slot;
@@ -90,7 +98,9 @@ CREATE TABLE IF NOT EXISTS slot(
   slot_day DATE NOT NULL,
   slot_hour TIME NOT NULL,
   room_id TINYINT(3) NOT NULL,
-  movie_id INT(5) NOT NULL
+  movie_id INT(5) NOT NULL,
+  CONSTRAINT fk_slot_room_id FOREIGN KEY (room_id) REFERENCES room (room_id),
+  CONSTRAINT fk_slot_movie_id FOREIGN KEY (movie_id) REFERENCES movie (movie_id)
 ) ENGINE = InnoDB;
 
 DROP TABLE IF EXISTS resa;
@@ -102,5 +112,8 @@ CREATE TABLE IF NOT EXISTS resa(
   children_price_place INT(2) NOT NULL,
   customer_id INT(7) NOT NULL,
   movie_id INT(5) NOT NULL,
-  slot_id INT(11) NOT NULL
+  slot_id INT(11) NOT NULL,
+  CONSTRAINT fk_resa_customer_id FOREIGN KEY (customer_id) REFERENCES customer (customer_id),
+  CONSTRAINT fk_resa_movie_id FOREIGN KEY (movie_id) REFERENCES movie (movie_id),
+  CONSTRAINT fk_resa_slot_id FOREIGN KEY (slot_id) REFERENCES slot (slot_id)
 ) ENGINE = InnoDB;
